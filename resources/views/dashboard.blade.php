37b5d032d3ef71a868c663772d979c1d08db372c
MaweedDV <desarrollopuertomontt@gmail.com>
2023-10-12 06:32:37 +0200

    update dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,19 +19,71 @@
     </div>
     <div class="content">
         <div class="row items-push">
+
+            <div class="col-md-12">
+                <a class="block block-rounded block-link-shadow" href="javascript:void(0)">
+                  <div class="block-content block-content-full">
+                    <div class="row text-center">
+                      <div class="col-4 border-end">
+                        <div class="py-3">
+                          <div class="item item-circle bg-body-light mx-auto">
+                            <i class="fa fa-3x fa fa-clipboard-list text-primary"></i>
+                          </div>
+                          <p class="fs-3 fw-medium mt-3 mb-0">
+                            {{$surveys_count}}
+                          </p>
+                          <p class="text-muted mb-0">
+                            Encuestas completadas
+                          </p>
+                        </div>
+                      </div>
+                      <div class="col-4 border-end">
+                        <div class="py-3">
+                          <div class="item item-circle bg-body-light mx-auto">
+                            <i class="fa fa-3x fa fa-book-open text-primary"></i>
+                          </div>
+                          <p class="fs-3 fw-medium mt-3 mb-0">
+                            {{$frs_count}}
+                          </p>
+                          <p class="text-muted mb-0">
+                            Formularios Reclamos, Sugerencias y/o Felicitaciones
+                          </p>
+                        </div>
+                      </div>
+                      <div class="col-4">
+                        <div class="py-3">
+                          <div class="item item-circle bg-body-light mx-auto">
+                            <i class="fa fa-3x fa fa-users text-primary"></i>
+                          </div>
+                          <p class="fs-3 fw-medium mt-3 mb-0">
+                            {{$users_count}}
+                          </p>
+                          <p class="text-muted mb-0">
+                            Usuarios
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            <h3>Reclamos, Sugerencias y Felicitaciones</h3>
             <div class="col-md-6 col-xl-4">
-                <a class="block block-rounded block-link-shadow bg-danger" href="javascript:void(0)">
+                <a class="block block-rounded block-link-shadow bg-success" href="javascript:void(0)">
                     <div class="block-content block-content-full d-flex align-items-center justify-content-between">
                         <div class="me-3">
                             <p class="text-white fs-3 fw-medium mb-0">
-                                {{$surveys}}
+                                {{$felicitaciones_count}}
                             </p>
                             <p class="text-white-75 mb-0">
-                                Encuestas
+                                FELICITACIONES
+                            </p>
+                            <p class="text-white fs-3 fw-medium mb-0">
+                                {{$felicitaciones_porcent}}%
                             </p>
                         </div>
                         <div>
-                            <i class="fa fa-2x fa-boxes text-black-50"></i>
+                            <i class="fa fa-2x fa-circle-check text-black-50"></i>
                         </div>
                     </div>
                 </a>
@@ -41,80 +93,96 @@
                     <div class="block-content block-content-full d-flex align-items-center justify-content-between">
                         <div class="me-3">
                             <p class="text-white fs-3 fw-medium mb-0">
-
-                                {{$frs}}
+                                {{$sugerencias_count}}
                             </p>
                             <p class="text-white-75 mb-0">
-                                Reclamos, Sugerencias y Felcitaciones
+                                SUGERENCIAS
+                            </p>
+                            <p class="text-white fs-3 fw-medium mb-0">
+                                {{$sugerencias_porcent}}%
                             </p>
                         </div>
                         <div>
-                            <i class="fa fa-2x fa-boxes text-black-50"></i>
+                            <i class="fa fa-2x fa-envelope-open-text text-black-50"></i>
                         </div>
                     </div>
                 </a>
             </div>
             <div class="col-md-6 col-xl-4">
-                <a class="block block-rounded block-link-shadow bg-success" href="javascript:void(0)">
+                <a class="block block-rounded block-link-shadow bg-danger" href="javascript:void(0)">
                     <div class="block-content block-content-full d-flex align-items-center justify-content-between">
                         <div class="me-3">
                             <p class="text-white fs-3 fw-medium mb-0">
-                                {{$sugerencias}}
+                                {{$reclamos_count}}
                             </p>
                             <p class="text-white-75 mb-0">
-                                Ganancias
+                                RECLAMOS
+                            </p>
+                            <p class="text-white fs-3 fw-medium mb-0">
+                                {{$reclamos_porcent}}%
                             </p>
                         </div>
                         <div>
-                            <i class="fa fa-2x fa-boxes text-black-50"></i>
+                            <i class="fa fa-2x fa-circle-xmark text-black-50"></i>
                         </div>
                     </div>
                 </a>
             </div>
-            <div class="col-md-6 col-xl-6">
-                <div class="block block-rounded h-100 mb-0">
-                    <div class="block-header block-header-default">
-                        <h3 class="block-title">
-                            Welcome to your app
-                        </h3>
+
+            <h3>Satisfacción encuestas</h3>
+            <div class="col-md-6 col-xl-4">
+                <a class="block block-rounded block-link-shadow bg-success" href="javascript:void(0)">
+                    <div class="block-content block-content-full d-flex align-items-center justify-content-between">
+                        <div class="me-3">
+                            <p class="text-white fs-3 fw-medium mb-0">
+                                {{round($satisfaccion_max_porcent)}}%
+                            </p>
+                            <p class="text-white-75 mb-0">
+                                Muy satisfactorio
+                            </p>
+                        </div>
+                        <div>
+                            <i class="fa fa-2x fa-circle-check text-black-50"></i>
+                        </div>
                     </div>
-                    <div class="block-content">
-                        <p>
-                            We’ve put everything together, so you can start working on your Laravel project as soon as
-                            possible! Dashmix assets are integrated and work seamlessly with Laravel Vite, so you can use
-                            the npm scripts as you would in any other Laravel project.
-                        </p>
-                        <p>
-                            Feel free to use any examples you like from the full HTML version to build your own pages.
-                        </p>
-                        <p class="fw-semibold">
-                            Wish you all the best and happy coding!
-                        </p>
-                    </div>
-                </div>
+                </a>
             </div>
-            <div class="col-md-6 col-xl-6">
-                <div class="block block-rounded h-100 mb-0">
-                    <div class="block-header block-header-default">
-                        <h3 class="block-title">
-                            Welcome to your app
-                        </h3>
+            <div class="col-md-6 col-xl-4">
+                <a class="block block-rounded block-link-shadow bg-warning" href="javascript:void(0)">
+                    <div class="block-content block-content-full d-flex align-items-center justify-content-between">
+                        <div class="me-3">
+                            <p class="text-white fs-3 fw-medium mb-0">
+                                {{round($satisfaccion_med_porcent)}}%
+                            </p>
+                            <p class="text-white-75 mb-0">
+                                Medianamente satisfactorio
+                            </p>
+                        </div>
+                        <div>
+                            <i class="fa fa-2x fa-envelope-open-text text-black-50"></i>
+                        </div>
                     </div>
-                    <div class="block-content">
-                        <p>
-                            We’ve put everything together, so you can start working on your Laravel project as soon as
-                            possible! Dashmix assets are integrated and work seamlessly with Laravel Vite, so you can use
-                            the npm scripts as you would in any other Laravel project.
-                        </p>
-                        <p>
-                            Feel free to use any examples you like from the full HTML version to build your own pages.
-                        </p>
-                        <p class="fw-semibold">
-                            Wish you all the best and happy coding!
-                        </p>
+                </a>
+            </div>
+            <div class="col-md-6 col-xl-4">
+                <a class="block block-rounded block-link-shadow bg-danger" href="javascript:void(0)">
+                    <div class="block-content block-content-full d-flex align-items-center justify-content-between">
+                        <div class="me-3">
+                            <p class="text-white fs-3 fw-medium mb-0">
+                                {{round($satisfaccion_min_porcent)}}%
+                            </p>
+                            <p class="text-white-75 mb-0">
+                                Muy insatisfactorio
+                            </p>
+                        </div>
+                        <div>
+                            <i class="fa fa-2x fa-circle-xmark text-black-50"></i>
+                        </div>
                     </div>
-                </div>
+                </a>
             </div>
         </div>
     </div>
 @endsection
+
+
